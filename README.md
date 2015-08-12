19cc5131c6276ef99d593170ca9ebbf0ebf39ef6
Jason Madigan <mad@redhat.com>
2015-08-12 14:18:38 +0100

    Initial

---
+++ README.md
@@ -0,0 +1,10 @@
+# SAML Client
+
+This is an example SAML Client App, designed to be used in conjunction with our sample SAML Service
+
+## Cordova Platforms
+
+For this example, we're using Cordova 3.5.x - additional platforms can also be added, we've just added iOS and Android via:
+
+		cordova platform add android@3.5.1
+		cordova platform add ios@3.5.0
---
+++ platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,34 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "id": "cordova-plugin-device.device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-dialogs/www/notification.js",
+        "id": "cordova-plugin-dialogs.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+        "id": "cordova-plugin-inappbrowser.inappbrowser",
+        "clobbers": [
+            "cordova.InAppBrowser.open",
+            "window.open"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-device": "1.0.1",
+    "cordova-plugin-dialogs": "1.1.1",
+    "cordova-plugin-inappbrowser": "1.0.1"
+}
+// BOTTOM OF METADATA
+});
